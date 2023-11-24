1e5134dffd5e8891537ded3a2ec8296bb8e7929a
inessahi <ines.sahie@gmail.com>
2023-11-24 08:03:32 +0700

    klik button "Add to cart" di tab Woman

--- cypress/e2e/Magento/choose product.cy.js
+++ cypress/e2e/Magento/choose product.cy.js
@@ -1,16 +1,33 @@
+const { select } = require("async");
+
 describe('template spec', () => {
   it('passes', () => {
     //visit ke web utama
-    cy.visit('https://magento.softwaretestingboard.com')
-    cy.get("#ui-id-4").click();
-    //pilih buttongroup 'Women',pilih dropdown 'Tops' --> 'Jackets'
-    //cy.visit('https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html')
-    
-    //klik salah satu item produk
-    //cy.visit('https://magento.softwaretestingboard.com/olivia-1-4-zip-light-jacket.html')
-    
-    //pilih size,color, qty, kemudian klik button 'add to cart'
-    //cy.get('button[title="Add To Card"]').click();
+    cy.visit('https://magento.softwaretestingboard.com');
+
+    //pilih buttongrup "Woman"
+    cy.get('#ui-id-4').click();
+
+    //Pilih produk yg nama produk "Radiant Tee"
+    cy.get('[title="Radiant Tee"]').click();
+
+    //pilih size "XS"
+    cy.get('#option-label-size-143-item-166').click();
+
+    //Pilih color "blue"
+    cy.get('#option-label-color-93-item-50').click();
+
+    //hapus textbox qty
+    cy.get('#qty').clear();
+
+
+  //input qty=2
+    cy.get('#qty').type('2');
+
+//klik button Add to cart
+    cy.get('button[title="Add to Cart"]').click();
+
+
   })
   
 })
